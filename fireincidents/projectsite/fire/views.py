b558b89ee01970acb3e26dcc9c56118b1c85e429
angegevela <202280079@psu.palawan.edu.ph>
2025-04-16 18:38:29 +0800

    successful chart setup and dynamic pie chart

--- fireincidents/projectsite/fire/views.py
+++ fireincidents/projectsite/fire/views.py
@@ -3,10 +3,41 @@ from django.views.generic.list import ListView
 from .models import Locations, Incident, FireStation
 
 
+from django.db import connection
+from django.http import JsonResponse
+from django.db.models.functions import ExtractMonth
+
+
+from django.db.models import Count
+from datetime import datetime
+
+
 class HomePageView(ListView):
     model = Locations
     context_object_name = 'home'
     template_name = "home.html"
+
+class ChartView(ListView):
+    template_name = 'chart.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+    def get_queryset(self, *args, **kwargs):
+        pass
+
+def PieCountbySeverity(request):
+    query = '''
+    SELECT severity_level, COUNT(*) as count
+    FROM fire_incident
+    GROUP BY severity_level
+    '''
+    with connection.cursor() as cursor:
+        cursor.execute(query)
+        rows = cursor.fetchall()
+
+    data = {severity: count for severity, count in rows} if rows else {}
+    return JsonResponse({'ok': True})
 
 
 def map_station(request):
--- fireincidents/projectsite/projectsite/urls.py
+++ fireincidents/projectsite/projectsite/urls.py
@@ -1,12 +1,19 @@
 from django.contrib import admin
 from django.urls import path
 
-from fire.views import HomePageView
+from fire.views import HomePageView, ChartView, PieCountbySeverity
 from fire import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', HomePageView.as_view(), name='home'),
-    path('stations', views.map_station, name='map-station')
+    path('dashboard_chart', ChartView.as_view(), name='dashboard-chart'),
+    path('chart/', PieCountbySeverity, name='chart'),
 
+
+
+
+
+
+    path('stations', views.map_station, name='map-station'),
 ]
